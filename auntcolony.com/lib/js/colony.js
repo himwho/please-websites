8c365c9c0e0678fe2cd60438fb151935b7d62cf4
Dylan Marcus <dylan@mach1studios.com>
2020-03-16 12:56:19 -0400

    auntcolony: some cleanup and QoL

--- auntcolony.com/lib/js/colony.js
+++ auntcolony.com/lib/js/colony.js
@@ -1,14 +1,22 @@
-function myMove() {
-  var elem = document.getElementById("animate");   
-  var pos = 0;
-  var id = setInterval(frame, 5);
-  function frame() {
-    if (pos == 350) {
-      clearInterval(id);
-    } else {
-      pos++; 
-      elem.style.top = pos + 'px'; 
-      elem.style.left = pos + 'px'; 
-    }
+var $circle = $('.ant'),
+    $wrapper = $('#wrapper'); 
+
+  function moveCircle(e) {
+    TweenLite.to($circle, 0.3, {
+      css: {
+        left: e.pageX,
+        top: e.pageY
+      }
+    });
   }
-}
+
+var flag = false;
+$($wrapper).mouseover(function(){
+  flag = true;
+  TweenLite.to($circle,0.4,{scale:1,autoAlpha:1})
+  $($wrapper).on('mousemove', moveCircle);
+});
+$($wrapper).mouseout(function(){
+    flag = false;
+    TweenLite.to($circle,0.4,{scale:0.1,autoAlpha:0})
+});
